e98c9fec965b8923ca31a2cd42236fb0f0a6a945
Didier Roche <didrocks@ubuntu.com>
2015-08-30 16:10:07 +0200

    First Time To Smile commit
    Enable checking for camera permission

---
+++ app/src/main/java/fr/teamrocks/timetosmile/MainActivity.java
@@ -0,0 +1,97 @@
+package fr.teamrocks.timetosmile;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.Snackbar;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final int REQUEST_CAMERA_PERM = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get Camera permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            createCameraSource();
+        } else {
+            requestCameraPermission();
+        }
+
+    }
+
+    /**
+     * Handles the requesting of the camera permission.  This includes
+     * showing a "Snackbar" message of why the permission is needed then
+     * sending the request.
+     */
+    private void requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted. Requesting permission");
+
+        final Activity thisActivity = this;
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Log.w(TAG, "Was already denied, giving more rationale");
+
+            View.OnClickListener listener = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ActivityCompat.requestPermissions(thisActivity, permissions, REQUEST_CAMERA_PERM);
+                }
+            };
+
+            Snackbar.make(findViewById(R.id.rootView), getString(R.string.camera_perm_rationale),
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(getString(R.string.camera_perm_rationale_OK), listener)
+            .show();
+        } else
+            ActivityCompat.requestPermissions(thisActivity, permissions, REQUEST_CAMERA_PERM);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+    }
+
+        private void createCameraSource() {
+        Log.w(TAG, "Create a camera source");
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
